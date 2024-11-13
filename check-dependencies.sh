04f0fdd492bb11872e0e877e83e30fc1e8252cea
tpaau-17DB <mikolaj_archusr@tutamail.com>
2024-11-13 21:47:54 +0100

    Patched check-dependencies.sh

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -1,12 +1,11 @@
 echo -n "g++: "
 command -v g++ >/dev/null 2>&1 && echo "OK" || echo "NO"
 
+echo -n "make: "
+command -v make >/dev/null 2>&1 && echo "OK" || echo "NO"
+
 echo -n "ncurses: "
 pkg-config --exists ncurses && echo "OK" || echo "NO"
 
 echo -n "nlohmann/json: "
-if [ -f "$(find /usr/include -name 'json.hpp')" ]; then
-  echo "OK"
-else
-  echo "NO"
-fi
+find /usr/include -name 'json.hpp' > /dev/null 2>&1 && echo "OK" || echo "NO"
