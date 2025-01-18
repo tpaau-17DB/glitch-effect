89c76f62a00d5e0af41b1d73712b648f0ea96917
tpaau-17DB <mikolaj_archusr@tutamail.com>
2025-01-18 22:12:41 +0100

    Experimenting with `automake`.

--- cleanup.sh
+++ cleanup.sh
@@ -3,21 +3,10 @@
 TO_REMOVE=(
     "aclocal.m4"
     "autom4te.cache"
-    "config.h"
-    "config.h.in"
-    "config.log"
-    "config.status"
-    "Makefile"
-    "depcomp"
-    "install-sh"
-    "missing"
     "stamp-h1"
     "configure~"
-    "configure"
-    "compile"
     "src/.deps/"
     "src/.dirstamp"
-    "Makefile.in"
 )
 
 echo "Cleaning up..."
